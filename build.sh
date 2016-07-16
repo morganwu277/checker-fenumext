02285346dfad0da3f0d735f595a5b71251a8e2ad
morgan.wu <xue777hua@gmail.com>
2016-07-16 13:37:53 -0400

    if condition

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ build_result=$?
 rm -rf dist
 mkdir dist
 
-if [[ "$build_result" == "0" ]]; then
+if [ "$build_result" == "0" ]; then
   cp target/checker-fenumext-*.jar dist/
   cp fenumext-intdef-check.sh dist/
   cp fenumext-stringdef-check.sh dist/
