b7ec635a921a206fcc3bda1ce6813a3f014ac46b
morgan.wu <xue777hua@gmail.com>
2016-07-16 13:42:44 -0400

    if condition

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 mvn clean package 
 build_result=$?
 rm -rf dist
 mkdir dist
 
-if [ "$build_result" == "0" ]; then
+if [ "${build_result}" = "0" ]; then
   cp target/checker-fenumext-*.jar dist/
   cp fenumext-intdef-check.sh dist/
   cp fenumext-stringdef-check.sh dist/
